524586e9dd016c18ee011649c41e512d3594eb48
mppior <142513218+mppior@users.noreply.github.com>
2026-05-05 13:50:51 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// Test configuration - DO NOT USE IN PRODUCTION
+const config = {
+  aws_access_key_id: 'AKIAIOSFODNN7EXAMPLE',
+  aws_secret_access_key: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+  github_token: 'ghp_R2D2C3POBBv8Y7example1234567890ab',
+  database_password: 'SuperSecret123!',
+  api_key: 'sk-proj-abcdefghijklmnopqrstuvwxyz123456'
+};
+
+module.exports = config;
